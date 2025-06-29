a941d97a258ac446d23bdecbb05f5450414e75c6
Connorisdunn <97138356+Connorisdunn@users.noreply.github.com>
2025-06-28 19:39:49 -0600

    Add files via upload

---
+++ project/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import Connect from './pages/Connect';
+import Experience from './pages/Experience';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/connect" element={<Connect />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
