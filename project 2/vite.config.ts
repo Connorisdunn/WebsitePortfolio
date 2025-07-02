bffcb1b906b332e21cc1ff6c9c70b5d36e49f96f
Connorisdunn <97138356+Connorisdunn@users.noreply.github.com>
2025-07-01 23:13:26 -0600

    Update vite.config.ts

--- project 2/vite.config.ts
+++ project 2/vite.config.ts
@@ -1,10 +1,10 @@
+// In project 2/vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/WebsitePortfolio/', // Replace 'your-repo-name' with your actual GitHub repository name
+  base: '/WebsitePortfolio/', // Your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
