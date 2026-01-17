3e48c8858850e1b9749f302d57a3adb639cd2bb9
Connorisdunn <97138356+Connorisdunn@users.noreply.github.com>
2026-01-17 11:46:59 -0700

    Update Experience.tsx

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -77,7 +77,7 @@ const Experience: React.FC = () => {
                         <img 
                           src={edu.schoolLogo} 
                           alt={`${edu.school} logo`}
-                          className="w-[200%] h-[200%] object-contain"
+                          className="w-[500%] h-[500%] object-contain"
                           onError={(e) => {
                             e.currentTarget.style.display = 'none';
                           }}
@@ -127,7 +127,7 @@ const Experience: React.FC = () => {
                         <img 
                           src={exp.companyLogo} 
                           alt={`${exp.company} logo`}
-                          className="w-[200%] h-[200%] object-contain"
+                          className="w-[500%] h-[500%] object-contain"
                           onError={(e) => {
                             e.currentTarget.style.display = 'none';
                           }}
