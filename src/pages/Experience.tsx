260b63ef361cf24ba5c3a1b7b7579fe98d9d8e8d
Connorisdunn <97138356+Connorisdunn@users.noreply.github.com>
2026-01-17 13:58:55 -0700

    Update Experience.tsx

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -7,7 +7,7 @@ const Experience: React.FC = () => {
     {
       title: "AP Engineer",
       company: "Tesla",
-      companyLogo: "https://upload.wikimedia.org/wikipedia/commons/b/bd/Tesla_Motors.svg",
+      companyLogo: `${import.meta.env.BASE_URL}tesla_wordmark.svg`,
       location: "Draper, UT",
       period: "April 2025 - Current",
       bullets: [
