d0fc9771e564077abba0b0d14d75842149eabe82
Connorisdunn <97138356+Connorisdunn@users.noreply.github.com>
2026-01-17 10:42:04 -0700

    Resize logo container in Experience component

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -73,7 +73,7 @@ const Experience: React.FC = () => {
                   <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-8">
                     {/* Logo - MUCH BIGGER */}
                     <div className="flex-shrink-0">
-                      <div className="w-48 h-48 sm:w-56 sm:h-56 bg-white border-2 border-black p-6 flex items-center justify-center">
+                      <div className="w-32 h-32 sm:w-40 sm:h-40 bg-white border-2 border-black p-6 flex items-center justify-center">
                         <img 
                           src={edu.schoolLogo} 
                           alt={`${edu.school} logo`}
